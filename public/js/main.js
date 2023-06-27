142867defe265e229bce1cce617d7fb6fa861688
Rocken <r0ck3n@hotmail.com>
2023-06-27 14:46:54 +0200

    ajot d'un rappel sur le call API

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,8 @@
-console.log("test");
+/*petit exemple de call API*/
+(async function townInfo() {
+  const townName = "bordeaux";
+  const town = await fetch(
+    `https://geo.api.gouv.fr/communes?nom=${townName}`
+  ).then((response) => response.json());
+  console.log(town);
+})();
